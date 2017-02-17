13d1b4cfac78e5c7499983e238b2ba45452de125
ErikBoesen <erik.boesen@gmail.com>
2017-02-16 20:05:19 -0500

    Add event data point selection

--- tba.go
+++ tba.go
@@ -5,7 +5,9 @@ import (
     "fmt"
     "os"
     "strings"
+    "strconv"
     "regexp"
+    "time"
 
     "github.com/ErikBoesen/tba"
 )
@@ -56,16 +58,20 @@ func main() {
     // Check which subcommand was Parsed using the FlagSet.Parsed() function; then handle each case accordingly.
     // FlagSet.Parse() will evaluate to false if no flags were parsed (i.e. the user didn't provide any flags).
     if teamCommand.Parsed() {
+        // Team key
+        tk := *teamNumber
 
         // Fetch team data
-        team, _ := tba.GetTeam(*teamNumber)
+        team, _ := tba.GetTeam(tk)
+
+        // TODO: Catch nonexistent teams
 
         // Trim URL fragments from start of event website URL
         team.Website = urlRE.ReplaceAllString(team.Website, "")
 
         // If user didn't specify a data point, output all of them.
         if *teamDataPoint == "" {
-            fmt.Printf("\n    Team %d:\n", *teamNumber)
+            fmt.Printf("\n    Team %d:\n", tk)
             fmt.Printf("\tNickname: %s\n", team.Nickname)
             //fmt.Printf("\tName/Sponsors: %s\n", team.Name)
             fmt.Printf("\tWebsite: %s\n", team.Website)
@@ -82,38 +88,46 @@ func main() {
         } else {
             switch strings.ToLower(*teamDataPoint) {
             case "name":
-                fmt.Printf("Team %d's name: %s\n", *teamNumber, team.Name)
+                fmt.Printf("\n    Team %d's name: %s\n\n", team.TeamNumber, team.Name)
             case "website":
-                fmt.Printf("Team %d's website: %s\n", *teamNumber, team.Website)
+                fmt.Printf("\n    Team %d's website: %s\n\n", team.TeamNumber, team.Website)
         	case "locality":
-                fmt.Printf("Team %d's locality: %s\n", *teamNumber, team.Locality)
+                fmt.Printf("\n    Team %d is based in %s.\n\n", team.TeamNumber, team.Locality)
         	case "rookieyear":
-                fmt.Printf("Team %d's first year was: %d\n", *teamNumber, team.RookieYear)
+                fmt.Printf("\n    Team %d's first year was %d.\n\n", team.TeamNumber, team.RookieYear)
         	case "region":
-                fmt.Printf("Team %d plays in the %s region.\n", *teamNumber, team.Region)
+                fmt.Printf("\n    Team %d plays in the %s region.\n\n", team.TeamNumber, team.Region)
             case "teamnumber":
-                fmt.Printf("Team %d's number is %d.\n", *teamNumber, team.TeamNumber)
+                fmt.Printf("\n    Team %d's number is %d.\n\n", team.TeamNumber, team.TeamNumber)
         	case "location":
-                fmt.Printf("Team %d comes from %s.\n", *teamNumber, team.Location)
+                fmt.Printf("\n    Team %d is based in %s.\n\n", team.TeamNumber, team.Location)
         	case "key":
-                fmt.Printf("Team %d's key is %s.\n", *teamNumber, team.Key)
-        	case "countryname":
-                fmt.Printf("Team %d is from the following country: %s\n", *teamNumber, team.CountryName)
+                fmt.Printf("\n    Team %d's key is %s.\n\n", team.TeamNumber, team.Key)
+        	case "country":
+                fmt.Printf("\n    Team %d is from %s.\n\n", team.TeamNumber, team.CountryName)
         	case "motto":
-                fmt.Printf("Team %d's motto is \"%s\".\n", *teamNumber, team.Motto)
+                if string(team.Motto[0]) == "\"" {
+                    fmt.Printf("\n    Team %d's motto is %s.\n\n", team.TeamNumber, team.Motto)
+                } else {
+                    fmt.Printf("\n    Team %d's motto is \"%s\".\n\n", team.TeamNumber, team.Motto)
+                }
         	case "nickname":
-                fmt.Printf("Team %d's nickname is %s.\n", *teamNumber, team.Nickname)
+                fmt.Printf("\n    Team %d's nickname is %s.\n\n", team.TeamNumber, team.Nickname)
             default:
-                fmt.Printf("Unrecognized data point \"%s\".\n", *teamDataPoint)
+                fmt.Printf("\n    Invalid data point \"%s\".\n\n", *teamDataPoint)
             }
         }
     }
     if eventCommand.Parsed() {
+        ek := *eventKey
+        if _, err := strconv.Atoi(string((*eventKey)[0])); err != nil {
+            ek = fmt.Sprintf("%d%s", time.Now().Year(), *eventKey)
+        }
 
-        event, _ := tba.GetEvent(*eventKey)
+        event, _ := tba.GetEvent(ek)
 
         if event.Key == "" {
-            fmt.Printf("\n    Invalid event key \"%s\".\n\n", *eventKey)
+            fmt.Printf("\n    Invalid event key \"%s\".\n\n", ek)
             os.Exit(1)
         }
 
@@ -123,13 +137,20 @@ func main() {
         event.StartDate = strings.Replace(event.StartDate, "-", "/", -1)
         event.EndDate = strings.Replace(event.EndDate, "-", "/", -1)
 
+        event.VenueAddress = strings.Replace(event.VenueAddress, "\n", ", ", -1)
+
         if *eventDataPoint == "" {
-            fmt.Printf("\n    %d %s (%s):\n", event.Year, event.Name, event.Key)
+            fmt.Printf("\n    %d %s (%s):\n\n", event.Year, event.Name, event.Key)
             //fmt.Printf("\tShortname: %s\n", event.ShortName)
             if event.StartDate == event.EndDate {
                 fmt.Printf("\tDate: %s\n", event.StartDate)
             } else {
                 fmt.Printf("\tDates: %s - %s\n", event.StartDate, event.EndDate)
+            }
+            if event.Official {
+                fmt.Println("\tOfficial: Yes")
+            } else {
+                fmt.Println("\tOfficial: No")
             }
             fmt.Printf("\tTimezone: %s\n", event.Timezone)
             fmt.Printf("\tWebsite: %s\n", event.Website)
@@ -137,19 +158,50 @@ func main() {
                 fmt.Printf("\tDistrict: %s (ID %d)\n", event.EventDistrictString, event.EventDistrict)
             }
             fmt.Printf("\tLocation: %s\n", event.Location)
-            fmt.Printf("\tAddress: %s\n", strings.Replace(event.VenueAddress, "\n", ", ", -1))
+            fmt.Printf("\tAddress: %s\n", event.VenueAddress)
             fmt.Printf("\tEvent Type: %s (ID %d)\n\n", event.EventTypeString, event.EventType)
-
-            /*Official            bool          `json:"official"`
-            ShortName           string        `json:"short_name"`
-            FacebookEid         interface{}   `json:"facebook_eid"`
-            Webcast             []interface{} `json:"webcast"`
-            Alliances           []struct {
-                Declines []interface{} `json:"declines"`
-                Picks    []string      `json:"picks"`
-            } `json:"alliances"`*/
         } else {
-            fmt.Println("Event data point fetching coming soon, for now just get the whole event.")
+            switch strings.ToLower(*eventDataPoint) {
+            case "key":
+                fmt.Printf("\n    Event %s's key is %s.\n\n", event.Name, event.Key)
+            case "name":
+                fmt.Printf("\n    Event %s's full name is %s.\n\n", event.Name, event.Name)
+            case "shortname":
+                fmt.Printf("\n    Event %s's shortname is %s.\n\n", event.Name, event.ShortName)
+            case "website":
+                fmt.Printf("\n    Event %s's website can be found at %s\n\n", event.Name, event.Website)
+            case "date":
+            case "dates":
+                if event.StartDate == event.EndDate {
+                    fmt.Printf("\tThe %s takes place on %s.\n", event.Name, event.StartDate)
+                } else {
+                    fmt.Printf("\tThe %s takes place from %s to %s.\n", event.Name, event.StartDate, event.EndDate)
+                }
+            case "startdate":
+                fmt.Printf("\n    The %s starts on %s.\n\n", event.Name, event.StartDate)
+            case "enddate":
+                fmt.Printf("\n    The %s ends on %s.\n\n", event.Name, event.EndDate)
+            case "official":
+                if event.Official {
+                    fmt.Printf("\n    The %s is an official FIRST event.\n\n", event.Name)
+                } else {
+                    fmt.Printf("\n    The %s is not an official FIRST event.\n\n", event.Name)
+                }
+        	case "district":
+                fmt.Printf("\n    The %s is part of the %s district (ID %d).\n\n", event.Name, event.EventDistrictString, event.EventDistrict)
+        	case "location":
+                fmt.Printf("\n    The %s is based in %s.\n\n", event.Name, event.Location)
+            case "year":
+                fmt.Printf("\n    The %s took place in %d.\n\n", event.Name, event.Year)
+            case "timezone":
+                fmt.Printf("\n    The %s is in the %s timezone.\n\n", event.Name, event.Timezone)
+            case "address":
+                fmt.Printf("\n    The %s took place at %s.\n\n", event.Name, event.VenueAddress)
+            case "type":
+                fmt.Printf("\n    The %s is a %s (ID %d).\n\n", event.Name, event.EventTypeString, event.EventType)
+            default:
+                fmt.Printf("\n    Invalid data point \"%s\".\n\n", *eventDataPoint)
+            }
         }
     }
 }
